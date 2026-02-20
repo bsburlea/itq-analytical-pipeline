ed4c9f3837e0fbb7e7cf912a406fdd1ee16a6778
Bogdan Sburlea <bsburlea@gmail.com>
2026-02-20 13:43:23 -0500

    Add minimal package entrypoint (python -m itq.run)

---
+++ src/itq/run.py
@@ -0,0 +1,13 @@
+"""
+Minimal entrypoint for ITQ analytical pipeline.
+"""
+
+from itq import data_utils
+
+
+def main():
+    print("ITQ analytical pipeline entrypoint")
+
+
+if __name__ == "__main__":
+    main()
